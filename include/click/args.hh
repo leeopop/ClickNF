0922b7e4633e7bed5584c462412b36d2c7d4dd52
Keunhong Lee <dlrmsghd@gmail.com>
2019-05-23 14:45:40 +0900

    Add compatibility layer to clicknf header

--- include/click/args.hh
+++ include/click/args.hh
@@ -1,6 +1,28 @@
 // -*- c-basic-offset: 4; related-file-name: "../../lib/args.cc" -*-
 #ifndef CLICK_ARGS_HH
 #define CLICK_ARGS_HH
+
+//#define ENABLE_MICROBENCH
+#ifdef ENABLE_MICROBENCH
+#include <R/microbench.hpp>
+#endif
+
+#ifndef DO_MICROBENCH
+#define DO_MICROBENCH()
+#endif
+
+#ifndef DO_MICROBENCH_WITH_INTERVAL
+#define DO_MICROBENCH_WITH_INTERVAL(interval)
+#endif
+
+#ifndef DO_MICROBENCH_WITH_NAME
+#define DO_MICROBENCH_WITH_NAME(name)
+#endif
+
+#ifndef DO_MICROBENCH_WITH_NAME_INTERVAL
+#define DO_MICROBENCH_WITH_NAME_INTERVAL(name, interval)
+#endif
+
 #include <click/type_traits.hh>
 #include <click/vector.hh>
 #include <click/string.hh>
