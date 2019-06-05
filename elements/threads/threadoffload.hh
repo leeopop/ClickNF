33964de5dbad572daa54b3a80fe82f4ac6ceb689
Keunhong Lee <dlrmsghd@gmail.com>
2019-06-05 15:56:36 +0900

    wip

--- elements/threads/threadoffload.hh
+++ elements/threads/threadoffload.hh
@@ -14,12 +14,12 @@ public:
     }Annotation;
 
     static inline ThreadOffload::Annotation* get_anno(Packet* p) {
-        ThreadOffload::Annotation* ptr = (ThreadOffload::Annotation*)p->anno_u8(Packet::AllAnnoSize);
+        ThreadOffload::Annotation* ptr = (ThreadOffload::Annotation*)((uintptr_t)p->anno() + Packet::AllAnnoSize);
         return ptr;
     }
 
     static inline void set_anno(Packet* p, ThreadOffload::Annotation* source) {
-        ThreadOffload::Annotation* ptr = (ThreadOffload::Annotation*)p->anno_u8(Packet::AllAnnoSize);
+        ThreadOffload::Annotation* ptr = (ThreadOffload::Annotation*)((uintptr_t)p->anno() + Packet::AllAnnoSize);
         memcpy(ptr, source, sizeof(ThreadOffload::Annotation));
     }
 
