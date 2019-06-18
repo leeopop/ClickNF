07cf17f29ebc5b52a877c366bbf559d8e9d1b9d1
Keunhong Lee <dlrmsghd@gmail.com>
2019-06-18 16:44:59 +0900

    make fix arp module

--- elements/ethernet/fixedarp.cc
+++ elements/ethernet/fixedarp.cc
@@ -20,26 +20,38 @@ FixedArp::~FixedArp()
 {
 }
 
-int
-FixedArp::configure(Vector<String> &conf, ErrorHandler *errh)
+int FixedArp::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-    ArgContext context(this);
+	ArgContext context(this);
 
-	if (conf.size() != 2) {
+	if (conf.size() != 2)
+	{
 		errh->error("expected SRC DST");
-	    return -1;
+		return -1;
 	}
 	if (EtherAddressArg().parse(conf[0], src, context))
-			return -1;
+		return -1;
 
 	if (EtherAddressArg().parse(conf[1], dst, context))
-			return -1;
+		return -1;
 	return 0;
 }
 
-Packet * FixedArp::simple_action(Packet *p)
+Packet *FixedArp::simple_action(Packet *p)
 {
-   return p;
+	// make room for Ethernet header.
+	assert(!p->shared());
+	WritablePacket *q;
+	q = p->uniqueify();
+	q = p->push_mac_header(sizeof(click_ether));
+	q->ether_header()->ether_type = htons(ETHERTYPE_IP);
+
+	EtherAddress *dst_eth = reinterpret_cast<EtherAddress *>(q->ether_header()->ether_dhost);
+	EtherAddress *src_eth = reinterpret_cast<EtherAddress *>(q->ether_header()->ether_shost);
+
+	memcpy(&q->ether_header()->ether_dhost, dst.data(), 6);
+	memcpy(&q->ether_header()->ether_shost, src.data(), 6);
+	return q;
 }
 
 CLICK_ENDDECLS
